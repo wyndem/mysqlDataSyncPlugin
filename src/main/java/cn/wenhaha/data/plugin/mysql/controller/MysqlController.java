5d663154b928d5afd3b1286e5e0b6940cf346d5b
wyndem <wyndem@qq.copm>
2022-07-31 20:20:04 +0800

    修改添加数据库接口

--- src/main/java/cn/wenhaha/data/plugin/mysql/controller/MysqlController.java
+++ src/main/java/cn/wenhaha/data/plugin/mysql/controller/MysqlController.java
@@ -2,9 +2,11 @@ package cn.wenhaha.data.plugin.mysql.controller;
 
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.exceptions.ExceptionUtil;
 import cn.hutool.db.Db;
 import cn.hutool.db.Entity;
 import cn.hutool.db.ds.simple.SimpleDataSource;
+import cn.hutool.db.sql.SqlExecutor;
 import cn.wenhaha.data.plugin.mysql.DataSourceUtil;
 import cn.wenhaha.data.plugin.mysql.MysqlContext;
 import cn.wenhaha.data.plugin.mysql.bean.MysqlSource;
@@ -13,13 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static cn.hutool.json.XMLTokener.entity;
 
 @RestController
 @RequestMapping("mysql")
@@ -29,31 +28,37 @@ public class MysqlController {
 
 
     @PostMapping("add")
-    public String add(@RequestBody MysqlSource mysql,@RequestHeader(value = "userId",required = false) Long userId) throws SQLException {
+    public String add(@RequestBody MysqlSource mysql,@RequestHeader(value = "userId",required = false) Long userId) {
 
-        int count = MysqlContext.db.count(Entity.create("user")
-                        .set("address",mysql.getAddress())
-                        .set("port",mysql.getPort())
-                        .set("name",mysql.getName())
-                        .set("table",mysql.getTable()));
-        if(count!=0){
-            return "该账号已经被添加了";
+
+
+        try{
+            int count = MysqlContext.db.count(Entity.create("user")
+                    .set("address",mysql.getAddress())
+                    .set("port",mysql.getPort())
+                    .set("name",mysql.getName())
+                    .set("table",mysql.getTable()));
+            if(count!=0){
+                return "该账号已经被添加了";
+            }
+            DataSourceUtil.initDataSource(mysql);
+            String s = Db.use(mysql.getDataSource()).queryString("select version();");
+            log.info("数据库版本{}",s);
+
+            Entity entity = Entity.parse(mysql)
+                    .set("create_id", userId)
+                    .set("create_time", DateUtil.now())
+                    .set("last_update", DateUtil.now());
+            entity.remove("dataSource");
+            int rows = MysqlContext.db.insert(entity.setTableName("user"));
+            if (rows==0){
+                return "ERROR_插入失败";
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return  e.getMessage();
         }
-        DataSourceUtil.initDataSource(mysql);
-        String s = Db.use(mysql.getDataSource()).queryString("select version();");
-        log.info("数据库版本{}",s);
-
-        Entity entity = Entity.parse(mysql)
-                .set("create_id", userId)
-                .set("create_time", DateUtil.now())
-                .set("last_update", DateUtil.now());
-
-        int rows = MysqlContext.db.insert(entity.setTableName("user"));
-        if (rows==0){
-            return "ERROR_插入失败";
-        }
-
-        return "OK";
+        return "ok";
     }
 
 
@@ -80,14 +85,22 @@ public class MysqlController {
     }
 
     @PostMapping("table")
-    public List<String> getTableName(@RequestBody TemMysqlInfo mysql) throws SQLException {
-        DataSource ds = new SimpleDataSource(mysql.getUrl(), mysql.getName(), mysql.getPassword());
-        return Db.use(ds)
-                .find(Collections.singletonList("TABLE_NAME"),
-                        Entity.create("information_schema.TABLES")
-                                .set("TABLE_SCHEMA", mysql.getTable())
-                ).stream().map(entity-> entity.getStr("TABLE_NAME")).collect(Collectors.toList());
+    public  Object getTableName(@RequestBody TemMysqlInfo mysql) {
 
+        try {
+            DataSource ds = new SimpleDataSource(mysql.getUrl(), mysql.getName(), mysql.getPassword());
+            ResultSet databases = SqlExecutor.callQuery(ds.getConnection(), "show databases;");
+            ArrayList<String> databasesList = new ArrayList<>();
+            while (databases.next()){
+                String string = databases.getString("Database");
+                databasesList.add(string);
+            }
+
+            return databasesList;
+
+        } catch (Exception e){
+            return ExceptionUtil.getSimpleMessage(e);
+        }
     }
 
 
